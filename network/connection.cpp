e0d9b4eec5fab2ea07a29e32a7729699ae22df7b
Ofer Ben-Yacov <ofer.benyacov@gmail.com>
2022-03-14 12:31:22 -0400

    disable degug printing

--- network/connection.cpp
+++ network/connection.cpp
@@ -11,8 +11,7 @@ int Connection::get_sock() {
 };
 
 void Connection::on_read() {
-    puts("got read event.");
-    // printf("thread id: %i\n",std::this_thread::get_id);
+    // puts("got read event.");
     
     ssize_t result;
     int i;
@@ -21,7 +20,7 @@ void Connection::on_read() {
     while (1) {
 
         result = ::recv(sd_, buf_, BUF_SIZE, 0);
-        printf("read result: %d\n", result);
+        // printf("read result: %d\n", result);
         if (result <= 0)
             break;
         connection_pair_->on_write(buf_, result);
@@ -35,7 +34,7 @@ void Connection::on_read() {
 };
 
 void Connection::on_write(char* buf, size_t size) {
-    printf("writing %i bytes to client %i\n", size, sd_);
+    // printf("writing %i bytes to client %i\n", size, sd_);
 
     int result = ::write(sd_, buf, size);
     if (result == -1) {
@@ -43,7 +42,7 @@ void Connection::on_write(char* buf, size_t size) {
         return;
     }
 
-    puts("writen data to client");    
+    // puts("writen data to client");    
     
 }
 
